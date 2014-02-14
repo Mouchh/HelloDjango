ad468c3a4f3060672627ffa89895a999d6d9e6e5
Cyril Mouchel <mouchel.cyril@gmail.com>
2014-02-14 22:37:13 +0100

    Aere le code

--- polls/views.py
+++ polls/views.py
@@ -6,20 +6,28 @@ from polls.models import Choice, Poll
 from polls.models import Poll
 
 def index(request):
+
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
+    
     return render(request, 'index.html', context)
 
 def detail(request, poll_id):
+    
     poll = get_object_or_404(Poll, pk=poll_id)
+    
     return render(request, 'detail.html', {'poll': poll})
 
 def results(request, poll_id):
+    
     poll = get_object_or_404(Poll, pk=poll_id)
+    
     return render(request, 'results.html', {'poll': poll})
 
 def vote(request, poll_id):
+    
     poll = get_object_or_404(Poll, pk=poll_id)
+    
     try:
         selected_choice = poll.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -34,4 +42,5 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
+    
     return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
