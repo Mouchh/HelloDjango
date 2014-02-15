227c05b871edf37f213b2cab046549fe79f1cb03
Cyril Mouchel <mouchel.cyril@gmail.com>
2014-02-15 16:42:36 +0100

    Utilisation des generic views

--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from polls.models import Choice, Poll
 
-from polls.models import Poll
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'latest_poll_list'
 
-def index(request):
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
 
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    
-    return render(request, 'index.html', context)
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'detail.html'
 
-def detail(request, poll_id):
-    
-    poll = get_object_or_404(Poll, pk=poll_id)
-    
-    return render(request, 'detail.html', {'poll': poll})
 
-def results(request, poll_id):
-    
-    poll = get_object_or_404(Poll, pk=poll_id)
-    
-    return render(request, 'results.html', {'poll': poll})
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'results.html'
 
 def vote(request, poll_id):
     
